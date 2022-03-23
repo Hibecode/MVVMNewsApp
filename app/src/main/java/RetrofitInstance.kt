669f292e51a1c76b9aa437a0aa36c6b7bd14dabf
Hibecode <ibkokunoye@gmail.com>
2022-03-23 23:52:06 +0100

    Room entity created

--- app/src/main/java/RetrofitInstance.kt
+++ app/src/main/java/RetrofitInstance.kt
@@ -5,15 +5,18 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class RetrofitInstance {
+
 
 
     companion object {
 
+
         private val retrofit by lazy {
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
+            val client= OkHttpClient.Builder()
                 .addInterceptor(logging)
                 .build()
             Retrofit.Builder()
@@ -22,6 +25,7 @@ class RetrofitInstance {
                 .client(client)
                 .build()
         }
+
 
         val api by lazy {
             retrofit.create(NewsAPI::class.java)
