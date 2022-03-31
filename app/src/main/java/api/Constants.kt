3b37f62d3e4985998f42e0ebeda82d94be1f0bf9
Hibecode <ibkokunoye@gmail.com>
2022-03-31 02:04:51 +0100

    done with search fragment and webViews

--- app/src/main/java/api/Constants.kt
+++ app/src/main/java/api/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
     companion object{
         const val API_KEY = "571f753783554d75b4cf6386e8ae6884"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 
 
